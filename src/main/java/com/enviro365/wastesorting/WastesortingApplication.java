219f5ea9a666196f6bc9d1f86c70f55a1b2ae86c
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2025-01-17 22:36:36 +0200

    I added the project structure from spring.io

---
+++ src/main/java/com/enviro365/wastesorting/WastesortingApplication.java
@@ -0,0 +1,13 @@
+package com.enviro365.wastesorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastesortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastesortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro365/wastesorting/WastesortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro365.wastesorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastesortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
