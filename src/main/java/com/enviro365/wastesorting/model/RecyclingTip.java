6acf312ab32e0b88279f8b3ab72299f517081281
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2025-01-18 19:17:32 +0200

    added the RecyclingTip model

--- src/main/java/com/enviro365/wastesorting/model/RecyclingTip.java
+++ src/main/java/com/enviro365/wastesorting/model/RecyclingTip.java
@@ -1,6 +1,5 @@
 package com.enviro365.wastesorting.model;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -8,21 +7,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
 @Getter
-@NoArgsConstructor
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
-public class WasteCategory {
-
+public class RecyclingTip {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
-    private String name;
+    private String tip;
 
-    @NotBlank
-    private String description;
+    @ManyToOne
+    private WasteCategory category;
 }
