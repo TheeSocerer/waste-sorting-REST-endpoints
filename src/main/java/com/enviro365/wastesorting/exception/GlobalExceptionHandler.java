71e4dbd2b28e516eb28c2877134229d140e0695c
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2025-01-19 19:31:57 +0200

    added the ErrorDetails class in payload package

--- src/main/java/com/enviro365/wastesorting/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro365/wastesorting/exception/GlobalExceptionHandler.java
@@ -1,2 +1,27 @@
 package com.enviro365.wastesorting.exception;
 
+
+import com.enviro365.wastesorting.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // handle specific exceptions
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
